c0d3a7b7463e2c945df3e76c30eda478e7f66c3a
Usmanzafar-123 <usmanzafar2411@gmail.com>
2024-09-26 19:10:20 +0530

    feat: careerconvo project

---
+++ models/post.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+    content: String,
+    dates:{
+      type:Date,
+      default:Date.now
+     },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }]
+});
+
+module.exports = mongoose.model('post', postSchema);
+
+
